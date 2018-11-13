bbb8dc3565d01599e59f272d627679882add8b08
Vishnu Anand <vishnu.anand@soti.net>
2018-11-13 18:15:04 +0530

    First Commit

---
+++ src/main/java/com/hairmaterials/config/DemoAppConfig.java
@@ -0,0 +1,14 @@
+package com.hairmaterials.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.hairmaterials")
+public class DemoAppConfig implements WebMvcConfigurer {
+
+}
+
